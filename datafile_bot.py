df2f8e7c2f28e1d375843aa16a0b0e9f62427357
greatzoo <gzuuus@protonmail.com>
2022-10-30 00:44:36 +0200

    init

---
+++ datafile_bot.py
@@ -0,0 +1,22 @@
+BOT_TOKEN='5767743049:AAHa3gw-uDGls-YesucaWNR7ZP6UMM-nuJs'
+
+dir_res='./resources/'
+dir_photos_output= './resources/output_photos/'
+dir_events_output='./resources/output_events/'
+
+font_name='Gogh-ExtraBold.ttf'
+hi_msg = '<u><b>Bienvenido al bot de 2140meetups</b></u>\n<a href="https://github.com/danielcharrua/bitcoin-2140-meetups">Guia para creacion de meetups</a>\n\nEste es un bot diseñado para ayudar a las comunidades a participar en la iniciativa de 2140meetups, de momento el bot tiene dos funcionalidades basicas:\n\n🖼️/createbadge Para crear el badge/foto de tu comunidad \n📢/gen_publication Para generar publicaciones rapidamente para los meetups de tu comunidad'
+color_list=['Blanco⬜', 'Negro⬛ ','Morado🟪', 'Rosa 🟣', 'Amarillo🟡', 'Verde 🟢', 'Azul🔵']
+event_cat_list=['📢General', '💬Socrático', '👾Dev']
+event_data={}
+outm_name='\n\n💡Evento: '
+outm_group='\n👥Grupo: '
+outm_description='\n\n📝Descripción: '
+outm_cat='\n\n🟠Categoría: '
+outm_type='\n🟢Tipo: '
+outm_location='\n📍Localización: '
+outm_location_link='\n🔗Link: '
+outm_date='\n\n🗓️Fecha: '
+outm_hour='\n🕓Hora: '
+lorem='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis leo nibh, accumsan ut dapibus tempus, ultricies eu turpis. Proin vitae massa eu tellus sodales mollis. Maecenas neque neque, tincidunt eget nunc nec, dapibus fringilla neque. '
+msg_ids=[]
