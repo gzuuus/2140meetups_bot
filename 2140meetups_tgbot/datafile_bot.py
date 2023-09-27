f67eaaf2ca44e7788444c5341f2c79c718de5b39
gzuuus <gzuuus@protonmail.com>
2023-09-27 13:19:45 +0200

    dump

---
+++ 2140meetups_tgbot/datafile_bot.py
@@ -0,0 +1,18 @@
+font_name='Gogh-ExtraBold.ttf'
+hi_msg = '<u><b>Bienvenido al bot de 2140meetups</b></u>\n<a href="https://2140meetups.com/guia/">Guia para creacion de meetups</a>\n\nEste es un bot diseñado para ayudar a las comunidades a participar en la iniciativa de 2140meetups \n\nFunciones del bot:\n🖼️/createbadge Crea un badge/foto de tu comunidad \n🔍/get_meetups Recibe publicaciones para los 3 proximos meetus o filtra por comunidad\n📰/feed Suscribete al feed para recibir actualizaciones regulares de los eventos publicados en la web\n🤠/get_feed No puedes esperar a que llegue la hora del feed? usa este comando para recibir el feed cuando quieras\n\n🟠Have fun and spread bitcoin!\n<a href="https://getalby.com/p/2140meetups">⚡2140meetups@getalby.com</a>'
+feed_msg= '<b>Feed info:</b>\n<u>Notificaciones:</u> \n🔸Meetups hoy, cada día comprueba si hay un meetup pasando ese mismo día y te lo muestra \n🔸Cada lunes y jueves meetups para los próximos 7 días\n🔸Cada 6h comprueba si se han añadido nuevos meetups\n🔸Cada 12h comprueba si se han añadido nuevas comunidades \n\n💡Si quieres cancelar tu suscripción solo haz /feed y pulsa el botón "Cancelar suscripción" \n\n<i>Si tienes alguna sugerencia o comentario hay un canal para esto <a href="https://t.me/meetups_bot_discussion"> 2140meetups bot discussion </a></i>'
+color_list=['Blanco⬜', 'Negro⬛','Morado🟪', 'Rosa 🟣', 'Amarillo🟡', 'Verde 🟢', 'Azul🔵']
+event_cat_list=['📢General', '💬Socrático', '👾Dev']
+event_data={}
+outm_name='\n\n💡Evento: '
+outm_group='\n👥Grupo: '
+outm_description='\n\n📝Descripción: '
+outm_cat='\n\n🟠Categoría: '
+outm_type='\n🟢Tipo: '
+outm_location='\n📍Localización: '
+outm_location_link='\n🔗Link: '
+outm_date='\n\n🗓️Fecha: '
+outm_hour='\n🕓Hora: '
+lorem='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis leo nibh, accumsan ut dapibus tempus, ultricies eu turpis. Proin vitae massa eu tellus sodales mollis. Maecenas neque neque, tincidunt eget nunc nec, dapibus fringilla neque. '
+msg_ids=[]
+m_separators="\n\n---\n\n"
