038bb57497f045a141555ef6913e24b3c3f44a52
Daniel Pereyra Costas <danielp@charrua.es>
2023-10-05 12:59:42 +0200

    updated messages

--- bot/constants.py
+++ bot/constants.py
@@ -26,7 +26,7 @@ timeframe_meetups="https://2140meetups.com/wp-json/wp/v2/meetup?from=%s&to=%s"
 community_meetups="https://2140meetups.com/wp-json/wp/v2/meetup?community=%s"
 
 # Messages
-hi_msg = '<u><b>Bienvenido al bot de 2140meetups</b></u>\n<a href="https://2140meetups.com/guia/">Guia para creacion de meetups</a>\n\nEste es un bot diseñado para ayudar a las comunidades a participar en la iniciativa de 2140meetups \n\nFunciones del bot:\n🖼️/createbadge Crea un badge/foto de tu comunidad \n🔍/get_meetups Recibe publicaciones para los 3 proximos meetus o filtra por comunidad\n📰/feed Suscribete al feed para recibir actualizaciones regulares de los eventos publicados en la web\n🤠/get_feed No puedes esperar a que llegue la hora del feed? usa este comando para recibir el feed cuando quieras\n\n🟠Have fun and spread bitcoin!\n<a href="https://getalby.com/p/2140meetups">⚡2140meetups@getalby.com</a>'
+hi_msg = '<u><b>Bienvenido al bot de 2140meetups</b></u>\n<a href="https://2140meetups.com/guia/">Guia para creacion de meetups</a>\n\nEste es un bot diseñado para ayudar a las comunidades a participar en la iniciativa de 2140meetups \n\n<b>Funciones del bot:</b>\n\n🔍 /get_meetups Recibe publicaciones para los 3 proximos meetus o filtra por comunidad\n📰 /feed Suscribete al feed para recibir actualizaciones regulares de los eventos publicados en la web\n🤠 /get_feed No puedes esperar a que llegue la hora del feed? usa este comando para recibir el feed cuando quieras\n\n🟠 Have fun and spread bitcoin!\n<a href="https://getalby.com/p/2140meetups">⚡ 2140meetups@getalby.com</a>'
 feed_msg= '<b>Feed info:</b>\n<u>Notificaciones:</u> \n🔸Meetups hoy, cada día comprueba si hay un meetup pasando ese mismo día y te lo muestra \n🔸Cada lunes y jueves meetups para los próximos 7 días\n🔸Cada 6h comprueba si se han añadido nuevos meetups\n🔸Cada 12h comprueba si se han añadido nuevas comunidades \n\n💡Si quieres cancelar tu suscripción solo haz /feed y pulsa el botón "Cancelar suscripción" \n\n<i>Si tienes alguna sugerencia o comentario hay un canal para esto <a href="https://t.me/meetups_bot_discussion"> 2140meetups bot discussion </a></i>'
 feed_subscription_msg='✅Te has suscrito al feed, ahora recibirás notificaciones sobre los meetups de 2140meetups.com'
 
@@ -73,8 +73,3 @@ WEEK=7
 lorem='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis leo nibh, accumsan ut dapibus tempus, ultricies eu turpis. Proin vitae massa eu tellus sodales mollis. Maecenas neque neque, tincidunt eget nunc nec, dapibus fringilla neque. '
 msg_ids=[]
 m_separators="\n\n---\n\n"
-
-# DEPRECATED
-badge="/createbadge"
-badge_msg="Crea un nuevo badge/insignia"
-color_list=['Blanco⬜', 'Negro⬛','Morado🟪', 'Rosa 🟣', 'Amarillo🟡', 'Verde 🟢', 'Azul🔵']
